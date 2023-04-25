30797b3ddd57f8405f4a5ba7533fc702471f8d59
Samuel Rodrigues dos Santos Cruz <samuel.santosc@sonda.com>
2023-04-25 01:38:41 -0300

    initializing project

---
+++ application/database/migrations/2023_04_25_042355_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->string('brand');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->integer('available_quantity');
+            $table->string('status',1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
